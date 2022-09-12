da98d6bc4378ee1b1d5320c88b178c1b344f9937
Sebastian Ferrari <sebas5384@gmail.com>
2022-09-12 19:55:56 -0300

    adding to check if the user is a member

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,41 @@
-import { useAddress, useMetamask } from "@thirdweb-dev/react";
+import { useAddress, useEditionDrop, useMetamask } from "@thirdweb-dev/react";
+import { useEffect, useState } from "react";
 
 const App = () => {
   const address = useAddress()
   const connectWithMetamask = useMetamask()
+  const editionDrop = useEditionDrop(process.env.REACT_APP_EDITION_DROP_ADDRESS);
+  const [isMember, setIsMember] = useState(false);
+
+  useEffect(() => {
+    // Exit if wallet is not connected.
+    if (!address) {
+      return;
+    }
+
+    const checkBalance = async () => {
+      const MEMBERSHIP_TOKEN_ID = 0;
+      try {
+        const balance = await editionDrop.balanceOf(address, MEMBERSHIP_TOKEN_ID);
+        if (balance.gt(0)) {
+          setIsMember(true)
+          console.log("🌟 this user has a membership NFT!");
+        }
+        else {
+          setIsMember(false)
+          console.log("😭 this user doesn't have a membership NFT.");
+        }
+      } catch (error) {
+        setIsMember(false)
+        console.error("Failed to get balance", error);
+      }
+    }
+
+    checkBalance();
+  }, [address, editionDrop])
+
+
+  
   console.log(`👋 Address: ${address}`)
 
   if (!address) {
