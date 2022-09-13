c3833e1a0587afcede119b9e43aa7567bdd4fcfc
Sebastian Ferrari <sebas5384@gmail.com>
2022-09-13 18:39:13 -0300

    adding script to deploy governance token

---
+++ scripts/5-deploy-token.js
@@ -0,0 +1,20 @@
+import { AddressZero } from "@ethersproject/constants";
+import sdk from "./1-initialize-sdk.js";
+
+(async () => {
+  try {
+    // Deploy a standard ERC-20 contract.
+    const tokenAddress = await sdk.deployer.deployToken({
+      name: "CodewarsDAO Governance Token",
+      symbol: "HACK",
+      primary_sale_recipient: AddressZero
+    })
+
+    console.log(
+      "✅ Successfully deployed token module, address:",
+      tokenAddress,
+    );
+  } catch (error) {
+    console.error("failed to deploy token module", error);
+  }
+})();
